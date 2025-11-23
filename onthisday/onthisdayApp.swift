fbb043777bd708ffa6dce473416a3f06f6ebdfe1
Kevin Rapkin <krapkin1@fau.edu>
2025-11-22 21:11:09 -0500

    Initial Commit

---
+++ onthisday/onthisdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  onthisdayApp.swift
+//  onthisday
+//
+//  Created by Kevin Rapkin on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct onthisdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
